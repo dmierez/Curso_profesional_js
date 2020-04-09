6d72e6b4f04793eb420bcf9bebba215a27bad93b
Diego Mierez <dmierez@gmail.com>
2020-04-08 22:36:44 -0300

    prueba del IntersectionObserver

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import MediaPlayer from './MediaPlayer.js';
-import AutoPlay from './assets/plugins/AutoPlay.js';
+import AutoPlay from './AutoPlay.js';
+import AutoPause from './AutoPause.js';
 
 
 const video = document.querySelector('video');
-const player = new MediaPlayer ({ el: video }) 
-    // plugins: [new AutoPlay()] })
+const player = new MediaPlayer ({ 
+    el: video,
+    plugins: [new AutoPlay(), new AutoPause()],
+ }) 
+
 
 const button = document.querySelector('button');
 button.onclick = () => player.togglePlay();
